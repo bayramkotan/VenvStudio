31d70d4c5958a1893cc6a6268b5406dd5480b505
bayramkotan <bayramkotan@gmail.com>
2026-04-17 16:22:05 +0300

    chore: bump version to 1.4.63

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -3,7 +3,7 @@ VenvStudio - Constants and Popular Package Catalog
 """
 
 APP_NAME = "VenvStudio"
-APP_VERSION = "1.4.62"
+APP_VERSION = "1.4.63"
 APP_DESCRIPTION = "Lightweight Python Virtual Environment Manager"
 APP_AUTHOR = "VenvStudio Team"
 
